e68ea1bda41f60947ec6a9033e182b8ea6a6857f
kevinasurjadi <gt_kevin_alexander@hotmail.com>
2022-02-28 00:44:09 +0700

    add firebase core dependency

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,10 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
 import 'flavors.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   F.appFlavor = Flavor.production;
+  await Firebase.initializeApp();
   runApp(const App());
 }

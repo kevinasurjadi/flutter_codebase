057468115f617bcea0ffcad909e251d41970e157
kevinasurjadi <gt_kevin_alexander@hotmail.com>
2022-03-11 22:08:33 +0700

    implement env

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_codebase/core/di/service_locator.dart';
 
 import 'app.dart';
-import 'flavors.dart';
+import 'core/di/service_locator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  F.appFlavor = Flavor.development;
   await Firebase.initializeApp();
   configureDependencies();
   runApp(const App());
